003f83966b685f80760d11417e901b4340b7c269
Кирилл Шемет <schemet.k.666@mail.ru>
2022-06-04 00:30:07 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+string test = "eUternjUHBJ<CBMN<SNCSOFJkdfvlkjdioutoeuweo;mczkxncjbxxl;vmx";
+test=Kata.Disemvowel(test);
+Console.WriteLine(test);
+
+public static class Kata
+{
+    public static string Disemvowel(string str)
+    {
+        string[] vowels = { "a", "e", "i", "o", "u", "A", "E", "I", "O", "U" };
+        for (int i = 0; i < vowels.Length; i++)
+        {
+            str = str.Replace(vowels[i], "");
+        }
+        return str;
+    }
+}
